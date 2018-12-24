ee9a774ac244cc7a168c3024ca12bfcf2ab90534
Dan Mandel <dan19926@gmail.com>
2018-12-24 11:37:27 -0500

    updated linting rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /**
- * @param name The name of what you're greeting.
+ * @param name The receiver of your greeting.
  * @param greeting The greeting to use.
  */
-export const greeter: Function = (name: string, greeting: string = 'hello'): string => `${greeting} ${name}`;
+export const greeter:Function = (name:string, greeting:string = 'hello'):string => `${greeting} ${name}`;
