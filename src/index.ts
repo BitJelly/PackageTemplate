cd179fb8df3944b3dba3cafc78cdf85ab072f2f1
Dan Mandel <dan19926@gmail.com>
2018-12-24 12:04:02 -0500

    updated jsdoc + rules

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 /**
- * @param name The receiver of your greeting.
- * @param greeting The greeting to use.
+ * A function used for generating greetings.
+ * @param  {string} name
+ * @param  {string='hello'} greeting
+ * @returns string
  */
 export const greeter:Function = (name:string, greeting:string = 'hello'):string => `${greeting} ${name}`;
