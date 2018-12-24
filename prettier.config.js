1b365eeced5673df2c8f04ff5b07f28d0f6d2c1e
Dan Mandel <dan19926@gmail.com>
2018-12-23 21:31:43 -0500

    init

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	printWidth: 100,
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	jsxBracketSameLine: false,
+	arrowParens: 'avoid',
+	proseWrap: 'always'
+};
